58f34bda39afc62df6025aa7881c1504d90d8382
Lucas Alves <lucasalves_taz@hotmail.com>
2024-06-22 13:48:13 -0300

    up actions.py | models.py

--- SRC/api/models.py
+++ SRC/api/models.py
@@ -1,11 +1,19 @@
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Float, Integer
+
 from .database import BASE
 
 
-class User(BASE):
-    __tablename__ = "Dadapredict"
+class Datavar(BASE):
+    __tablename__ = 'Datavar'
 
     id = Column(Integer, primary_key=True, index=True)
     tamanho = Column(Integer)
     quartos = Column(Integer)
     vagas = Column(Integer)
+
+
+class Datapred(BASE):
+    __tablename__ = 'Datapred'
+
+    id = Column(Integer, primary_key=True, index=True)
+    previsao = Column(Float)
