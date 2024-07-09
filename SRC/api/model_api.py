256a7429ba7359ddb7fb9f9700374575d1b5735e
Lucas Alves <lucasalves_taz@hotmail.com>
2024-07-09 20:15:32 -0300

    model_api.py

--- SRC/api/model_api.py
+++ SRC/api/model_api.py
@@ -1,11 +1,10 @@
 from http import HTTPStatus
 
 import joblib
-from fastapi import Depends, FastAPI
-from sqlalchemy.orm import Session
+from fastapi import FastAPI
 
-from . import actions, models, schemas
-from .database import SessionLocal, engine
+from . import models, schemas
+from .database import engine
 
 models.BASE.metadata.create_all(bind=engine)
 
@@ -13,13 +12,14 @@ app = FastAPI()
 
 modelo = joblib.load('model.pkl')
 
-
+"""
 def Acessdb():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+"""
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=schemas.DataMessage)
@@ -27,9 +27,18 @@ def index():
     return {'message': 'API is running'}
 
 
+@app.post('/predict', status_code=HTTPStatus.CREATED)
+def predict(data: schemas.DataPredcit):
+    input_data = [[data.tamanho, data.quartos, data.vagas]]
+    predicition = modelo.predict(input_data)
+    return {'prediction': predicition[0]}
+
+
+"""
 @app.post('/predict/', status_code=HTTPStatus.CREATED)
 def predict(datavar: schemas.DataPredcit, db: Session = Depends(Acessdb)):
     features = [[datavar.tamanho, datavar.quartos, datavar.vagas]]
     preco_estimado = modelo.predict(features)[0]
     actions.insertDataVar(db=db, data=datavar)
     return {'preco_estimado': round(preco_estimado, 2)}
+"""
