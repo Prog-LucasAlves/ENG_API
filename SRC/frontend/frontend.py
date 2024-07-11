c5d5773581472d52655fbdd2f5e13c1739800e6f
Lucas Alves <lucasalves_taz@hotmail.com>
2024-07-10 23:53:05 -0300

    up frontend.py

--- SRC/frontend/frontend.py
+++ SRC/frontend/frontend.py
@@ -9,19 +9,28 @@ st.title('House Price Prediction')
 st.subheader('', divider='red')
 
 # Criando colunas aonde serão inseridos os dados.
-col1, col2, col3 = st.columns(3)
+(
+    col1,
+    col2,
+) = st.columns(2)
 
 with col1:
     tamanho = st.number_input('Tamanho (em m²)', min_value=0, max_value=500, value=20)
     st.write('The current number is ', tamanho)
 
-with col2:
+with col1:
     quartos = st.number_input(
         'Quartos (Quantidade)', min_value=0, max_value=10, value=0
     )
     st.write('The current number is ', quartos)
 
-with col3:
+with col2:
+    banheiros = st.number_input(
+        'Banheiros (Quantidade)', min_value=0, max_value=10, value=0
+    )
+    st.write('The current number is ', banheiros)
+
+with col2:
     vagas = st.number_input(
         'Vagas de Garagem (Quantidade)', min_value=0, max_value=10, value=0
     )
@@ -32,8 +41,10 @@ if st.button('Prever'):
     data = {
         'tamanho': tamanho,
         'quartos': quartos,
+        'banheiros': banheiros,
         'vagas': vagas,
     }
+
     # Acessando a api para realizar a previsão com os dados coletados
     response = requests.post('https://eng-api-e8wg.onrender.com/predict/', json=data)
     prediction = response.json()
