04785d913940095be3c5f1f73aadb865a11f8455
Lucas Alves <lucasalves_taz@hotmail.com>
2024-07-12 23:32:38 -0300

    up model_api.py

--- SRC/api/model_api.py
+++ SRC/api/model_api.py
@@ -34,20 +34,3 @@ def predict(data: schemas.DataPredcit, db: Session = Depends(Acessdb)):
     predicition = modelo.predict(input_data)
     actions.selectDataVar(db=db)
     return {'prediction': predicition[0]}
-
-
-"""
-@app.post('/predict', status_code=HTTPStatus.CREATED)
-def predict(data: schemas.DataPredcit):
-    input_data = [[data.tamanho, data.quartos, data.vagas]]
-    predicition = modelo.predict(input_data)
-    return {'prediction': predicition[0]}
-
-
-@app.post('/predict/', status_code=HTTPStatus.CREATED)
-def predict(datavar: schemas.DataPredcit, db: Session = Depends(Acessdb)):
-    features = [[datavar.tamanho, datavar.quartos, datavar.vagas]]
-    preco_estimado = modelo.predict(features)[0]
-    actions.insertDataVar(db=db, data=datavar)
-    return {'preco_estimado': round(preco_estimado, 2)}
-"""
