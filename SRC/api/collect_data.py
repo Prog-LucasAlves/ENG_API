3e249ecd7820a612d3100efa95af3e5d87377e6b
Lucas Alves <lucasalves_taz@hotmail.com>
2024-07-15 23:33:03 -0300

    up collect_data.py

--- SRC/api/collect_data.py
+++ SRC/api/collect_data.py
@@ -1,17 +1,11 @@
-from sqlalchemy.orm import Session
-
-from . import actions
-from .database import SessionLocal
+from actions import selectDataVar
+from database import SessionLocal
 
 
-def Acessdb():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-def Collectdata(db: Session = Acessdb):
-    data = actions.selectDataVar(db=db)
+def Collectdata():
+    data = selectDataVar(db=SessionLocal())
     data.to_csv('./data_var.csv', index=False)
+
+
+if __name__ == '__main__':
+    Collectdata()
